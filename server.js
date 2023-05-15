3c840a47c5b06924f8a5c372f148468e207b436f
Bunmi Oladipupo <bunmidavids194@gmail.com>
2023-05-15 10:18:38 +0100

    updated server

--- server.js
+++ server.js
@@ -13,32 +13,52 @@ const io = socketIO(server, {
   transports: ['websocket', 'polling']
 });
 
-let connectedUsers = {};
+let users = [];
 
+const addUser = (userId, socketId) => {
+  !users.some((user)=> user.userId === userId) && users.push({userId, socketId});
+  console.log("users joined: ", JSON.stringify(users))
+}
+
+const removeUser = (socketId) => {
+  users = users.filter((user)=> user.socketId !== socketId);
+  console.log("users left: ", JSON.stringify(users))
+}
 io.on('connection', (socket) => {
   console.log('A user connected');
-
-  socket.on('join', (data) => {
-    console.log('User joined room: ' + data.room);
-    socket.join(data.room);
-    connectedUsers[data.room] = connectedUsers[data.room] || [];
-    connectedUsers[data.room].push(data.username);
-    io.to(data.room).emit('userList', connectedUsers[data.room]);
+  io.emit("welcome", "welcome everyone to my chat APP");
+  socket.on('join', (userId) => {
+    // console.log('User joined room: ' + userId);
+    addUser(userId, socket.id);
+    io.emit('userList', users);
   });
 
   socket.on('message', (data) => {
+    console.log('data: ' + JSON.stringify(data));
+    console.log('recipient: ' + data.recipient);
     console.log('Message received: ' + data.content);
-    io.to(data.room).emit('message', data);
+    if (data.recipient) {
+      console.log(io.sockets);
+      io.to(data.recipient).emit('privateMessage', data.content);
+      const userSocket = io.sockets.connected[data.recipient];
+      if (userSocket) {
+        userSocket.emit('message', data);
+        console.log("message sent to " + data.recipient)
+      } else {
+        console.log(`User ${data.recipient} is not online`);
+        // Store the message in a database or file to be sent later
+      }
+    } else {
+      //do nothing
+      // io.to(data.room).emit('message', data);
+      console.log("nothing")
+    }
   });
 
   socket.on('disconnect', () => {
     console.log('A user disconnected');
-    Object.keys(connectedUsers).forEach((room) => {
-      if (connectedUsers[room].includes(socket.username)) {
-        connectedUsers[room].splice(connectedUsers[room].indexOf(socket.username), 1);
-        io.to(room).emit('userList', connectedUsers[room]);
-      }
-    });
+    removeUser(socket.id);
+    io.emit('userList', users);
   });
 });
 
